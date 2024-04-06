ce0a6ce33d4e411c197216e71e01f1f8e4ffab3e
tnguyen.neo <tnguyen.neo@gmail.com>
2024-04-06 11:53:23 +0700

    Add DevExtreme side nav outer toolbar layout'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,49 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './shared/components';
+import { AuthGuardService } from './shared/services';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { DxDataGridModule, DxFormModule } from 'devextreme-angular';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'tasks',
+    component: TasksComponent,
+    canActivate: [ AuthGuardService ]
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [ AuthGuardService ]
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [ AuthGuardService ]
+  },
+  {
+    path: 'login-form',
+    component: LoginFormComponent,
+    canActivate: [ AuthGuardService ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), DxDataGridModule, DxFormModule],
+  exports: [RouterModule],
+  providers: [
+    AuthGuardService
+  ],
+  declarations: [
+    HomeComponent,
+    ProfileComponent,
+    TasksComponent
+  ]
 })
 export class AppRoutingModule { }
